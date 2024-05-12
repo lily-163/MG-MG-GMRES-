69bc029d82775eaa25abceb34c4fcde67a33c574
杜黎 <63869772+lily-163@users.noreply.github.com>
2024-05-12 13:56:54 +0800

    Update Generate_f01.m

--- P2/Generate_f01.m
+++ P2/Generate_f01.m
@@ -1,7 +1,4 @@
 function int_fphi = Generate_f1(K1,K2,x,y)
-%         z = (K1+K2)*(-y.^2.*exp(x).*(y - 1))...
-%         + (K1+0.5*K2)*(-pi*sin(pi*x).*(3*y.^2 - 1))...
-%         + 0.5*K2*(-2*exp(x).*(3*y - 1));     % f0(1)²¿·Ö
         f11 = 0*x;
         for i = 1:length(f11)
             if x(i)>0.5 
